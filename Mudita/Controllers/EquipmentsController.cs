da6f1466b401fa6fd5f3560519874c9b2c4edfbe
Alex Shcherbakov <alexander@prolificinteractive.com>
2017-07-19 17:59:33 -0400

    Fix json

--- Mudita/Controllers/EquipmentsController.cs
+++ Mudita/Controllers/EquipmentsController.cs
@@ -16,35 +16,35 @@ namespace Mudita.Controllers
                 {
                     ["id"] = "124",
                     ["title"] = "Sheathed resistance bands",
-                    ["imageUrl"] =
+                    ["imageUrlPath"] =
                     "https://68.media.tumblr.com/f23aeb4aef2fd9de04a5ff589a20b7d0/tumblr_o8k5maYQLr1tjukx2o2_250.jpg"
                 },
                 new Dictionary<string, string>
                 {
                     ["id"] = "2435",
                     ["title"] = "Continuous Looped Bands",
-                    ["imageUrl"] =
+                    ["imageUrlPath"] =
                     "https://68.media.tumblr.com/d26314ab932bef741c38fd772dee64a9/tumblr_oe26f8JLrE1tjukx2o3_540.jpg"
                 },
                 new Dictionary<string, string>
                 {
                     ["id"] = "3456",
                     ["title"] = "Exercise Ball",
-                    ["imageUrl"] =
+                    ["imageUrlPath"] =
                     "https://68.media.tumblr.com/067f9504deb1fe224c3ffa97b549da0b/tumblr_oj622btrvL1uoz5lzo2_400.jpg"
                 },
                 new Dictionary<string, string>
                 {
                     ["id"] = "3457",
                     ["title"] = "Medicine ball",
-                    ["imageUrl"] =
+                    ["imageUrlPath"] =
                     "https://68.media.tumblr.com/3970b1c9550508aa34e9639be0c4f70c/tumblr_o8mtwgfFwK1tcb9uto2_540.jpg"
                 },
                 new Dictionary<string, string>
                 {
                     ["id"] = "7345",
                     ["title"] = "Kettlebell",
-                    ["imageUrl"] =
+                    ["imageUrlPath"] =
                     "https://68.media.tumblr.com/3970b1c9550508aa34e9639be0c4f70c/tumblr_o8mtwgfFwK1tcb9uto2_540.jpg"
                 },
                 new Dictionary<string, string>
